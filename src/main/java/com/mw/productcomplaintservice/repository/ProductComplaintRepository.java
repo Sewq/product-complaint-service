c863e2c891e51abe970f04fa8a40217049b8a3d1
Sewq <sewilion@gmail.com>
2025-03-30 15:12:10 +0200

    remove unused method and organise imports

--- src/main/java/com/mw/productcomplaintservice/repository/ProductComplaintRepository.java
+++ src/main/java/com/mw/productcomplaintservice/repository/ProductComplaintRepository.java
@@ -1,7 +1,6 @@
 package com.mw.productcomplaintservice.repository;
 
 import com.mw.productcomplaintservice.model.ProductComplaint;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -13,8 +12,6 @@ public interface ProductComplaintRepository extends JpaRepository<ProductComplai
 
     @Query("SELECT p.id FROM ProductComplaint p WHERE p.productId = :productId AND p.complainerName = :complainerName")
     Optional<UUID> findUuidByProductIdAndComplainerName(String productId, String complainerName);
-
-    Optional<ProductComplaint> findByProductIdAndComplainerName(String productId, String complainerName);
 
     @Modifying
     @Query("UPDATE ProductComplaint e SET e.counter = e.counter + 1 WHERE e.id = :id")
